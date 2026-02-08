735c9e44dfab9d86d64f161614b146286ab645c6
MAK <denbor140@gmail.com>
2026-02-08 18:26:35 +0200

    add note hub

---
+++ app/notes/page.tsx
@@ -0,0 +1,29 @@
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query';
+import { fetchNotes } from '@/lib/api/api';
+import NotesClient from './Notes.client';
+
+const PER_PAGE = 12;
+
+interface NotesPageProps {
+  searchText: string;
+  page: number;
+}
+
+export default async function NotesPage({ searchText, page }: NotesPageProps) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', searchText, page],
+    queryFn: () => fetchNotes(searchText, page, PER_PAGE),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
+}
---
+++ lib/api/api.ts
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import type { Note } from '@/types/note';
+
+const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+export interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
+
+export const fetchNotes = async (
+  searchText: string,
+  page: number,
+  perPage: number
+) => {
+  const { data } = await axios.get<FetchNotesResponse>('/notes', {
+    params: {
+      search: searchText,
+      page,
+      perPage,
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+export const createNote = async (
+  newNote: Pick<Note, 'title' | 'content' | 'tag'>
+) => {
+  const { data } = await axios.post<Note>('/notes', newNote, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+export const deleteNote = async (id: string) => {
+  const { data } = await axios.delete<Note>(`/notes/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+export const fetchNoteById = async (id: string): Promise<Note> => {
+  const { data } = await axios.get<Note>(`/notes/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
